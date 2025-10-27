15651cf3a27bc4a70ad52a5c53ebe1281021d430
eshairfan2711-glitch <eshairfan.2711@gmail.com>
2025-10-27 19:14:58 +0500

    Add files via upload

---
+++ esha_portfolio_site/assets/js/main.js
@@ -0,0 +1,1 @@
+document.getElementById('year')?.textContent = new Date().getFullYear();document.getElementById('year2')?.textContent = new Date().getFullYear();document.getElementById('year3')?.textContent = new Date().getFullYear();document.getElementById('year4')?.textContent = new Date().getFullYear();document.getElementById('year5')?.textContent = new Date().getFullYear();
